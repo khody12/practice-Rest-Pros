319d5d24997a867b726cdb5456d6f9e8bf7478e5
khody <ekhodor8@gmail.com>
2024-12-24 13:12:42 -0800

    created a model called product, and a new app called product, api now accesses this info

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hi there, this is your django api response"})
+    # basically what we have now, is our py_client is 
+    model_data = Product.objects.all().order_by("?").first() # this would give random data, but i have the same price for my 2 prods rn
+    data = {}
+
+    if model_data:
+         # the tedious way
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data)
+
+#     
+# request is an HttpRequest from django
+#     #request.body
+#      # byte string of Json Data
+
+#     print(request.GET) # url query params
+#     print(request.POST)
+#     body = request.body
+    
+#     data = {}
+#     try:
+#         data = json.loads(body)
+#     except:
+#         pass
+#     print(data)
+
+    
+#     return JsonResponse(data)
 
 
 
-# Create your views here.
+# # Create your views here.
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,11 +5,15 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
+# we have our end point which is a certain url, we pass into requests.get, 
+# django will now take this url, and try to map a certain view to it, if there is a view
+# it will go inside that view and then do what the view says. right now, we have a view within api that imports data
+# from products, and it then returns the information to get_response. 
 
-get_response = requests.get(endpoint, json={"query":"Hello world"}) # Application programming interface
+get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"}) # Application programming interface
 
-print(get_response.text) # printing out the source code / raw text response that we got from our request
-print(get_response.status_code)
+#print(get_response.text) # printing out the source code / raw text response that we got from our request
+#print(get_response.status_code)
 
 # the difference between rest and regular requests
 
